d85935eb9647180ce1dc0579d7070af1fac3bb79
Finn Wallasch <wdog.rihpsalis@gmail.com>
2023-04-18 16:22:01 +0200

    Gitmap repaired

--- src/main/Gridmap.java
+++ src/main/Gridmap.java
@@ -5,58 +5,50 @@ import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
 
 import java.io.*;
-import java.sql.SQLOutput;
-import java.util.ArrayList;
 
 public class Gridmap { // txt einlesen und ränder hinzufügen
     private Image gridmap;
     private PixelReader pixelReader;
     private Image water, dirt, grass;
     private int terrainSize;
-    private int gridmapSizeX;
-    private int gridmapSizeY;
+    private int gridmapHeight;
+    private int gridmapWidth;
     private Player player;
     private double playerX;
     private double playerY;
     private FileReader fileReader;
     private BufferedReader bufferedReader;
-    private char[][] gridmapChar;
+    private char[][] gridmapChar2D;
+    private char[] gridmapChar1D;
+    private String buffer;
     public Gridmap(int size, Player player) {
-        gridmap = new Image("resources/terrain/floor/Gridmap3.png");
+        gridmap = new Image("resources/terrain/floor/Gridmap.png");
         terrainSize = size / 4;
         this.player = player;
-        gridmapSizeX = (int) gridmap.getWidth();
-        gridmapSizeY = (int) gridmap.getHeight();
-        gridmapChar = new char[gridmapSizeX] [gridmapSizeY];
-        System.out.println("Xsize: " + gridmapSizeX + "  Ysize: " + gridmapSizeY);
-
+        gridmapWidth = (int) gridmap.getWidth();
+        gridmapHeight = (int) gridmap.getHeight();
+        gridmapChar2D = new char[gridmapWidth][gridmapHeight];
         try {
-            fileReader = new FileReader("src/resources/terrain/floor/Gridmap3_values.txt");
+            fileReader = new FileReader("src/resources/terrain/floor/Gridmap_values.txt");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
         bufferedReader = new BufferedReader(fileReader);
-
-        for (int x = 0; x < gridmapSizeX; x++) {
-            for (int y = 0; y < gridmapSizeY; y++) {
-                try {
-                    gridmapChar[x][y] = (char) bufferedReader.read();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+        System.out.println("Height: " + gridmapHeight + " Width: " + gridmapWidth);
+        for (int i = 0; i < gridmapHeight; i++) {
             try {
-                bufferedReader.skip(1);
+                buffer = bufferedReader.readLine();
+                gridmapChar1D = buffer.toCharArray();
+                for (int j = 0; j < gridmapWidth; j++) {
+                    gridmapChar2D[j][i] = gridmapChar1D[j];
+                    System.out.print(gridmapChar2D[j][i]);
+                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }
-        for (int x = 0; x < gridmapSizeX; x++) {
-            for (int y = 0; y < gridmapSizeY; y++) {
-                System.out.print(gridmapChar[x][y]);
-            }
             System.out.println();
         }
+
     }
     public void render(GraphicsContext gcBg) {
         gcBg.drawImage(gridmap, playerX, playerY);
